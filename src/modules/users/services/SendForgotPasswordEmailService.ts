9ea420d7da19ca1267c2d1f70c6f3f6176b15b3c
Wellington Cid <wellington.cid@hotmail.com>
2020-09-30 19:26:12 -0300

    finalizing tests, saving tokens in db, and emails

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -5,8 +5,6 @@ import IMailProvider from '@shared/container/providers/MailProvider/models/IMail
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
-// import User from '../infra/typeorm/entities/User';
-
 interface IRequest {
   email: string;
 }
@@ -33,7 +31,7 @@ class SendForgotPasswordEmailService {
 
     await this.userTokensRepository.generate(checkUserExists.id);
 
-    this.mailProvider.sendMail(
+    await this.mailProvider.sendMail(
       email,
       'Pedido de recuperação de senha recebido',
     );

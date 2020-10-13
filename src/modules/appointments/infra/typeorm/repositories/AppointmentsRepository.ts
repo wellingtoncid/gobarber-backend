7b5f64954eab325f388a0b2f72912951d0b85aad
Wellington Cid <wellington.cid@hotmail.com>
2020-10-13 16:19:04 -0300

    adjustment order appointments

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -62,6 +62,7 @@ class AppointmentsRepository implements IAppointementsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      order: { date: 'ASC' },
       relations: ['user'],
     });
 
